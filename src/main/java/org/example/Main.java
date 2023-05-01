dd74a31e169d98ee744f322ab7898e0f82c9365f
Mmesomachithedeveloper <mmesomachithedeveloper@gmail.com>
2023-05-01 10:00:14 -0400

    Updated version

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,23 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.Stack;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Stack<String> stringStack = new Stack<>();
+
+        stringStack.push("Minerta");
+        stringStack.push("Mayonnaise");
+        stringStack.push("Magodo");
+        stringStack.push("Iwaya");
+
+        System.out.println(""+stringStack.isEmpty());
+
+        ///to remove an element, we can use the pop() method
+        String name = stringStack.pop();
+        System.out.println(stringStack);
+        System.out.println("The element removed is the last element added to the stack. Which is: "+name);
+        stringStack.search("Iwaya");
     }
 }
